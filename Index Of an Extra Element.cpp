6e2cc104e9bfd503eec2682564e3f517b42dee28
Preetam Singh <70262355+PreetamSingh18@users.noreply.github.com>
2021-10-12 17:36:12 +0530

    initial

---
+++ Index Of an Extra Element.cpp
@@ -0,0 +1,18 @@
+
+
+// Code BY Preetam Singh
+// Index Of an Extra Element
+ Question Link -> https://practice.geeksforgeeks.org/problems/index-of-an-extra-element/1
+
+
+
+class Solution{
+ public:
+    int findExtra(int a[], int b[], int n) {
+       for(int i=0;i<n;i++){
+           if(a[i]!=b[i]){
+               return i;
+           }
+       }
+    }
+};
